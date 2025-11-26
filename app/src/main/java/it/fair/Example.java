65cb0a3f0550255f6954a940f35d651f7739f0fc
fair <chowdhuryarpit9@gmail.com>
2025-11-26 20:45:55 +0100

    1 2 3! Via!

---
+++ app/src/main/java/it/fair/Example.java
@@ -0,0 +1,10 @@
+package it.fair;
+
+import it.fair.McCommands.Test;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.printf("%d\n", Test.addOne(10));
+    }
+}
